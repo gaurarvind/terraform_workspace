3cdac9731886c14703b2064c820d99eb9a5dd392
gaurarvind <89510703+gaurarvind@users.noreply.github.com>
2023-10-29 16:16:37 +0530

    ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "myec2" {
-    ami = "ami-0bb4c991fa89d4b9b"
-    instance_type = t2.micro
+    ami = "ami-04d1dcfb793f6fa37"
+    instance_type = "t2.micro"
 }
